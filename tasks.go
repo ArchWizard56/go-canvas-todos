b69a404bce607b37ca142756c46c854df6fff78e
Stephen Swanson <stephen.swanson@mailbox.org>
2023-09-30 14:55:02 -0700

    Added comments

--- tasks.go
+++ tasks.go
@@ -10,6 +10,7 @@ import (
 	"github.com/google/uuid"
 )
 
+// Task represents a task with its properties.
 type Task struct {
 	ID          string
 	Title       string
@@ -18,13 +19,19 @@ type Task struct {
 	DueDate     time.Time
 }
 
+// GetTasks retrieves tasks from a WebDAV calendar and returns them as a slice of Task.
 func GetTasks() []Task {
+	// Load configuration from config.json
 	config := LoadConfig("config.json")
+
+	// Create an HTTP client with basic authentication for WebDAV
 	c := webdav.HTTPClientWithBasicAuth(nil, config.DavUsername, config.DavPassword)
 	caldavClient, err := caldav.NewClient(c, config.DavURL)
 	if err != nil {
 		panic(err)
 	}
+
+	// Define a calendar component request and filter
 	compRequest := caldav.CalendarCompRequest{
 		Name: "VTODO",
 	}
@@ -33,11 +40,16 @@ func GetTasks() []Task {
 	}
 	query := caldav.CalendarQuery{CompRequest: compRequest, CompFilter: compFilter}
 
+	// Query the calendar for tasks
 	calendarObj, err := caldavClient.QueryCalendar("NDc", &query)
 	if err != nil {
 		panic(err)
 	}
+
+	// Initialize a slice to store tasks
 	tasks := []Task{}
+
+	// Iterate through calendar objects to extract tasks
 	for i := range calendarObj {
 		if calendarObj[i].Data.Name != "VCALENDAR" {
 			continue
@@ -46,7 +58,10 @@ func GetTasks() []Task {
 		if child.Name != "VTODO" {
 			continue
 		}
+
 		task := Task{}
+
+		// Extract task properties
 		if child.Props.Get("SUMMARY") != nil {
 			task.Title = child.Props.Get("SUMMARY").Value
 		}
@@ -55,6 +70,7 @@ func GetTasks() []Task {
 		}
 		task.ID = child.Props.Get("UID").Value
 		if child.Props.Get("DUE") != nil {
+			// Parse due date with two possible formats
 			task.DueDate, err = time.Parse("20060102T150405Z", child.Props.Get("DUE").Value)
 			if err != nil {
 				task.DueDate, err = time.Parse("20060102", child.Props.Get("DUE").Value)
@@ -66,29 +82,39 @@ func GetTasks() []Task {
 		if child.Props.Get("DESCRIPTION") != nil {
 			task.Description = child.Props.Get("DESCRIPTION").Value
 		}
+
+		// Append the task to the slice
 		tasks = append(tasks, task)
 	}
+
 	return tasks
 }
 
+// UpdateToDo updates a task in the WebDAV calendar.
 func UpdateToDo(id string, title string, calendar string, category string, description string, due time.Time) {
+	// Load configuration from config.json
 	config := LoadConfig("config.json")
+
+	// Create an HTTP client with basic authentication for WebDAV
 	c := webdav.HTTPClientWithBasicAuth(nil, config.DavUsername, config.DavPassword)
 	caldavClient, err := caldav.NewClient(c, config.DavURL)
 	if err != nil {
 		panic(err)
 	}
 
+	// Create a new iCalendar
 	vcal := ical.NewCalendar()
 	vcal.Props.Add(&ical.Prop{Name: "PRODID", Value: "Open-Xchange"})
 	vcal.Props.Add(&ical.Prop{Name: "VERSION", Value: "2.0"})
 	vcal.Name = "VCALENDAR"
 
+	// Create a new VTODO task
 	todo := ical.NewCalendar()
 	todo.Name = "VTODO"
-
 	todo.Props.Add(&ical.Prop{Name: "SUMMARY", Value: title})
 	todo.Props.Add(&ical.Prop{Name: "DTSTAMP", Value: time.Now().UTC().Format("20060102T150405Z")})
+
+	// Set DUE property if due date is not zero
 	if !due.IsZero() {
 		todo.Props.Add(&ical.Prop{Name: "DUE", Value: due.UTC().Format("20060102T150405Z")})
 	}
@@ -96,32 +122,42 @@ func UpdateToDo(id string, title string, calendar string, category string, descr
 	todo.Props.Add(&ical.Prop{Name: "DESCRIPTION", Value: description})
 	todo.Props.Add(&ical.Prop{Name: "UID", Value: id})
 
+	// Add the VTODO task to the VCALENDAR
 	vcal.Children = append(vcal.Children, todo.Component)
+
+	// Put the updated calendar object in WebDAV
 	_, err = caldavClient.PutCalendarObject(fmt.Sprintf("%s/%s.ics", calendar, id), vcal)
 	if err != nil {
 		panic(err)
 	}
 }
+
+// AddTodo adds a new task to the WebDAV calendar.
 func AddTodo(title string, calendar string, category string, description string, due time.Time) {
+	// Load configuration from config.json
 	config := LoadConfig("config.json")
+
+	// Create an HTTP client with basic authentication for WebDAV
 	c := webdav.HTTPClientWithBasicAuth(nil, config.DavUsername, config.DavPassword)
 	caldavClient, err := caldav.NewClient(c, config.DavURL)
 	if err != nil {
 		panic(err)
 	}
 
+	// Create a new iCalendar
 	vcal := ical.NewCalendar()
 	vcal.Props.Add(&ical.Prop{Name: "PRODID", Value: "Open-Xchange"})
 	vcal.Props.Add(&ical.Prop{Name: "VERSION", Value: "2.0"})
 	vcal.Name = "VCALENDAR"
 
+	// Create a new VTODO task
 	todo := ical.NewCalendar()
 	todo.Name = "VTODO"
-
 	id := uuid.New().String()
-
 	todo.Props.Add(&ical.Prop{Name: "SUMMARY", Value: title})
 	todo.Props.Add(&ical.Prop{Name: "DTSTAMP", Value: time.Now().UTC().Format("20060102T150405Z")})
+
+	// Set DUE property if due date is not zero
 	if !due.IsZero() {
 		todo.Props.Add(&ical.Prop{Name: "DUE", Value: due.UTC().Format("20060102T150405Z")})
 	}
@@ -129,7 +165,10 @@ func AddTodo(title string, calendar string, category string, description string,
 	todo.Props.Add(&ical.Prop{Name: "DESCRIPTION", Value: description})
 	todo.Props.Add(&ical.Prop{Name: "UID", Value: id})
 
+	// Add the VTODO task to the VCALENDAR
 	vcal.Children = append(vcal.Children, todo.Component)
+
+	// Put the new calendar object in WebDAV
 	_, err = caldavClient.PutCalendarObject(fmt.Sprintf("%s/%s.ics", calendar, id), vcal)
 	if err != nil {
 		panic(err)
